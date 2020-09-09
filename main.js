25e79e81370badf5e0643ddee9d0ce3a793fe580
YT Huang <ytzhbit@icloud.com>
2020-09-09 12:11:53 +0800

    Updates

--- main.js
+++ main.js
@@ -30,9 +30,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center',
+							'./Images/bg01.jpg': 'center',
+							'./Images/bg02.jpg': 'center',
+							'./Images/bg03.jpg': 'center',
 						},
 
 					// Delay.
